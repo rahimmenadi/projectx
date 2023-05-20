fc2fa9cd461f762a3981846f6251c48442ad904a
unknown <idow505@gmail.com>
2023-05-20 17:02:28 +0100

    dash biha b logic

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,40 +1,51 @@
-// Obtenez le canvas et créez le contexte
-var ctx = document.getElementById('myChart').getContext('2d');
+var data = {
+    jour: [20, 30, 40, 50,78],
+    semaine: [200, 250, 180, 300,200,183,152],
+    mois: [120, 150, 80, 200, 100],
+    annee: [1500, 2000, 1800, 2200, 1900, 199 , 271,1281,192,192,1627,1721],
+};
 
-// Créez le graphique
+var labels = {
+    jour: ['1PM', '2PM', '3PM', '4PM', '5PM'],
+    semaine: ['samedi','dimanche','Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi'],
+    mois: ['Première semaine', 'Deuxième semaine', 'Troisième semaine', 'Quatrième semaine'],
+    annee: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai' , 'juin' , 'juillet', 'aout' , 'septembre' , 'octobre' , 'novembre' , 'decemebre'],
+};
+
+var options = {
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    }
+};
+
+var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Juin'],
+        labels: labels.mois,
         datasets: [{
-            label: 'Ventes mensuelles',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            label: 'Ventes',
+            data: data.mois,
+            backgroundColor: 'rgba(0, 123, 255, 0.6)'
         }]
     },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
+    options: options
 });
+
+function updateChart() {
+    var selectedVariable = document.getElementById('selectVariable').value;
+    myChart.data.labels = labels[selectedVariable];
+    myChart.data.datasets[0].data = data[selectedVariable];
+    myChart.update();
+}
+
+//variables
+
+let userN = document.getElementById('userN');
+let totalS = document.getElementById('totalS');
+let totalP = document.getElementById('totalP');
+let totalR = document.getElementById('totalR');
+let totalV = document.getElementById('totalV');
+
